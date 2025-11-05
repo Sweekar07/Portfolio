6c2634983b18acffe44a9c8a9d29f5f8ae23c92e
Sweekar Bangera <sweekar.786b@gmail.com>
2025-11-05 18:34:40 +0530

    Hero section added social icons

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+
+
 import styles from "./Hero.module.css"
 import { getImageUrl } from "../../utils";
 
@@ -8,9 +12,20 @@ export const Hero = () => {
             <h2 className={styles.greeting}>Hi, my name is</h2>
             <h1 className={styles.title}>Sweekar Bangera.</h1>
             <h1 className={styles.subtitle}>I build things for the web and mobile.</h1>
-            <p 
+            <p
                 className={styles.description}>I’m a software engineer passionate about designing scalable backend systems while also contributing to the frontend to deliver seamless, end-to-end user experiences.
             </p>
+            <div className={styles.socialIcons}>
+                <a href="https://github.com/Sweekar07" target="_blank" rel="noopener noreferrer">
+                    <FaGithub size={28} />
+                </a>
+                <a href="https://linkedin.com/in/sweekar-bangera" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin size={28} />
+                </a>
+                <a href="https://leetcode.com/u/user1309BV/" target="_blank" rel="noopener noreferrer">
+                    <SiLeetcode size={28} />
+                </a>
+            </div>
         </div>
         <img src={getImageUrl("hero/heroImage.png")} alt="Hero Image of me" className={styles.heroImg} />
         <div className={styles.topBlur} />
